dd9c3905155ac2dd48b4a050817ca22bafa9de29
Rakibul Hasib <rakimhasib786@gmail.com>
2023-03-26 01:12:02 +0600

    Blazor Agent CRUD

--- MRMS-Blazor/MRMS-Blazor/MRMS-Blazor/Client/Program.cs
+++ MRMS-Blazor/MRMS-Blazor/MRMS-Blazor/Client/Program.cs
@@ -7,5 +7,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<AgentService>();
 await builder.Build().RunAsync();
